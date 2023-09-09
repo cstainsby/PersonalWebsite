7b8ff1ddf0b96f208b4286891ec5e299bbf8b4fe
cstainsby <cstainsby@zagmail.gonzaga.edu>
2023-09-09 15:07:53 -0700

    create abstract form component

--- frontend/src/components/forms/FormContainer.svelte
+++ frontend/src/components/forms/FormContainer.svelte
@@ -1,27 +1,38 @@
 
+
+<script lang="ts">
+
+    interface FormContainerProps {
+        
+    }
+</script>
+
+
 <style>
-    :global(form) {
+    .standard-form-container :global(form) {
         display: flex;
         flex-direction: column;
         max-width: 400px;
         margin: auto;
     }
   
-    :global(label) {
+    .standard-form-container :global(label) {
         display: block;
         margin-bottom: 10px;
     }
   
-    :global(input) {
+    .standard-form-container :global(input) {
         width: 100%;
         padding: 8px;
         margin-top: 4px;
         box-sizing: border-box;
     }
   
-    :global(button) {
+    .standard-form-container :global(button) {
         margin-top: 10px;
     }
 </style>
 
-<slot/>
+<div class="standard-form-container">
+    <slot/>
+</div>
